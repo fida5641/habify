b10db46c3d42272fa8e4ea565de1a16227b79f07
habify <fathimafidaetk@gmail.com>
2025-01-19 21:01:39 +0530

    working commit

---
+++ lib/view/constants.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// final TextStyle titleStyle = GoogleFonts.unbounded(
+//   fontWeight: FontWeight.w800,
+//   fontSize: 15,
+// );
+// Widget kHeight = SizedBox(height: 20);
+// Widget kWidth = SizedBox(width: 20);
+
+// const Color primaryColor = Color.fromARGB(255, 72, 41, 127);
+// const Color secondaryColor = Colors.white;
+// final Color grayShade = Colors.grey.shade800; 
